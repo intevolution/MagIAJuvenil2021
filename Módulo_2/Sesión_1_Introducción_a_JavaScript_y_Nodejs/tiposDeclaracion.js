f8ee535af8b451ff1f62be1e5b5290c88411c240
egarridoIntevolution <79933799+egarridoIntevolution@users.noreply.github.com>
2021-03-03 14:10:18 -0600

    Create tiposDeclaracion.js
    Código de la Sesión 1 del Módulo 2 - Introducción a JavaScript y Node.js

---
+++ Módulo_2/Sesión_1_Introducción_a_JavaScript_y_Nodejs/tiposDeclaracion.js
@@ -0,0 +1,24 @@
+//Código de la Sesión 1 del Módulo 2
+//Introducción a JavaScript y Node.js
+
+//Declaración de una Variable
+var numero = 365;
+console.log( numero );
+//Cambio de valor de la variable "numero"
+numero = 5000;
+//Impresión del valor de la variable "Numero" 
+console.log( numero );
+
+//Declaración de una Constante
+const mensaje = "Bienvenido a Ciudad Rio Verde";
+//Comentamos la línea donde tratamos de cambiar el valor de una constante
+//mensaje = "Bienvenido a Ciudad Rio Claro"; 
+//Impresión del valor de la constante "Mensaje"
+console.log( mensaje );
+
+//Características de una persona
+const nombre = "Diego";
+const apellidoPaterno = "Perez";
+const apellidoMaterno = "Dominguez";
+var edad = 15;
+const lugarNacimiento = "México";
