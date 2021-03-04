ed65ff3a51d63857b9050bd4d08ebef8ad672a19
egarridoIntevolution <79933799+egarridoIntevolution@users.noreply.github.com>
2021-03-03 19:28:42 -0600

    Update tiposDeclaracion.js
    Se agrega un ejercicio al final, el cual consiste en guardar las características de un coche.

--- Módulo_2/Sesión_1_Introducción_a_JavaScript_y_Nodejs/tiposDeclaracion.js
+++ Módulo_2/Sesión_1_Introducción_a_JavaScript_y_Nodejs/tiposDeclaracion.js
@@ -1,6 +1,9 @@
 //Código de la Sesión 1 del Módulo 2
 //Introducción a JavaScript y Node.js
 
+//Para ejecutar este programa, debemos poner en consola:
+//node tiposDeclaracion.js
+
 //Declaración de una Variable
 var numero = 365;
 console.log( numero );
@@ -22,3 +25,12 @@ const apellidoPaterno = "Perez";
 const apellidoMaterno = "Dominguez";
 var edad = 15;
 const lugarNacimiento = "México";
+
+//Ejercicio - Características de un Coche
+var placa = "HHF-453";
+const modelo = "Corolla";
+const marca = "Toyota";
+const paisOrigen = "Japón";
+const anio = 2010;
+//Impresión de la característica de un coche
+console.log( paisOrigen );
