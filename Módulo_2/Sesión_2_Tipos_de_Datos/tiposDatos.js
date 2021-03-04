ef6494cc365f81b255a2cef624dde5556b41a791
egarridoIntevolution <79933799+egarridoIntevolution@users.noreply.github.com>
2021-03-03 19:44:27 -0600

    Create tiposDatos.js

---
+++ Módulo_2/Sesión_2_Tipos_de_Datos/tiposDatos.js
@@ -0,0 +1,39 @@
+//Código de la Sesión 2 del Módulo 2
+//Tipos de datos
+
+//Para ejecutar este programa debemos poner en consola:
+//node tiposDatos.js
+
+//Tipo de dato "Number" - Números
+var numero1 = 5;
+var numero2 = 1500;
+
+//Tipo de dato "String" - Texto
+var texto1 = "Bicicleta";
+var texto2 = "Bienvenido al curso";
+
+//Tipo de dato "Boolean" - Valor verdadero o falso
+var booleanoVerdadero = true;
+var booleanoFalso = false;
+
+//Tipo de dato "Null" - Valor vacio
+var condicion = null;
+
+//Tipo de dato "Undefined" - Variable que todavía no tienen ningún valor asignado
+var sinValor;
+console.log( sinValor );
+
+//Tipo de dato "Array" - Colección de valores
+const arregloNumeros = [ 5000, 4200, 10, 30, 15, 10000 ];
+const arregloNombres = [ "Luis", "Erick", "Diego", "Ana"];
+const arregloMixto = [ 500, "Luis", 10, "Erick" ];
+
+//Tipo de dato "Object" - Objeto( Entidad de la vida real )
+//Objeto persona
+var persona = { 
+    nombre: "Diego",
+    apellidoPaterno: "Perez",
+    apellidoMaterno: "Dominguez",
+    edad: 15,
+    lugarNacimiento: "México" 
+}
