1737d5e3f3e33fb0075ae8d4bb32961259c9f464
egarridoIntevolution <79933799+egarridoIntevolution@users.noreply.github.com>
2021-03-09 08:22:04 -0600

    Update estructurasControlP1.js

--- Módulo_2/Sesión_4_Estructuras_de_Control/estructurasControlP1.js
+++ Módulo_2/Sesión_4_Estructuras_de_Control/estructurasControlP1.js
@@ -30,30 +30,38 @@ var oplN2 = 50;
 var oplN3 = 30;
 
 //Operador && ("AND" o "Y")
+//Ejemplo - Crear una condición compuesta de otras 2 condiciones, utilizando el operador && e imprimir el resultado
 var resultadoOperadorLY = ( oplN1 < oplN2 ) && ( oplN2 < oplN3 );
 console.log( "El resultado del operador Y, es: " + resultadoOperadorLY );
 
 //Operador || ("OR" o "O" )
+//Ejemplo - Crear una condición compuesta de otras 2 condiciones, utilizando el operador || e imprimir el resultado
 var resultadoOperadorLO = ( oplN1 < oplN2 ) || ( oplN2 < oplN3 );
 console.log( "El resultado del operador O, es: " + resultadoOperadorLO );
 
 //Operador de negación !
+//Ejemplo - Crear una condición compuesta de otras 2 condiciones, utilizando el operador ! e imprimir el resultado
 var resultadoOperadorLN = !resultadoOperadorLO;
 console.log( "El resultado del operador de Negación, es: " + resultadoOperadorLN );
 
+
+//Condicional if, else if y else
 var numero1 = 125;
 var numero2 = 50;
 var numero3 = 30;
 
-//Condicional IF - Dependiendo de la condición ejecutará las instrucciones dentro de las llaves {}.
+//Ejemplo, crear condiciones utilizando las variables declaradas anteriormente y 
+//desplegar un mensaje si se cumple la condición de cada una de ellas
+
+//Condicional if - Dependiendo de la condición ejecutará las instrucciones dentro de las llaves {}.
 if( (numero1 < numero2) && (numero2 < numero3) ){
     console.log( "Entraste al IF" );
 }
-//Condicional ELSE IF - Si no se ejecutaron las instrucciones dentro del IF, comprobará una nueva condición.
+//Condicional else if - Si no se ejecutaron las instrucciones dentro del IF, comprobará una nueva condición.
 else if( numero1 < numero3 ){
     console.log( "Entraste al ELSE IF" );
 }
-//ELSE -  Si no se ha cumplido ninguna condición, se ejecutarán estas instrucciones por defecto.
+//else -  Si no se ha cumplido ninguna condición, se ejecutarán estas instrucciones por defecto.
 else{
     console.log( "Entraste al ELSE" );
 }
