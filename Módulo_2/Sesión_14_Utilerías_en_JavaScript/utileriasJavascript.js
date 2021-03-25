c4e6244073f960ec7a38c939c3738c32e4c504e6
egarridoIntevolution <79933799+egarridoIntevolution@users.noreply.github.com>
2021-03-25 14:04:29 -0600

    Create utileriasJavascript.js

---
+++ Módulo_2/Sesión_14_Utilerías_en_JavaScript/utileriasJavascript.js
@@ -0,0 +1,19 @@
+//Código de la Sesión #14 Parte 2 del Módulo 2
+
+//Para ejecutar este programa debemos poner en consola:
+//node utileriasJavascript.js
+
+//Tema ----> Utilerías en JavaScript
+
+//Métodos de cadenas de texto
+var cadenaTexto = "Hola Lucia, buenas tardes Lucia";
+
+//Método length
+console.log( cadenaTexto.length );
+//Método indexOf
+console.log( cadenaTexto.indexOf( "Lucia" ) );
+//Método replace
+console.log( cadenaTexto.replace( "Lucia", "Angel" ) );
+//Método slice
+var nombre = cadenaTexto.slice( 5, 10 );
+console.log( nombre );
