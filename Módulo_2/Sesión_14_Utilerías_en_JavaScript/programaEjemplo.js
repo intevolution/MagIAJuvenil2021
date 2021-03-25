cf1d7a8a0f56fae2a69109b8a8dfb6c9f45869d2
egarridoIntevolution <79933799+egarridoIntevolution@users.noreply.github.com>
2021-03-25 14:08:27 -0600

    Create programaEjemplo.js

---
+++ Módulo_2/Sesión_14_Utilerías_en_JavaScript/programaEjemplo.js
@@ -0,0 +1,11 @@
+//Código de la Sesión #14 P1 del Módulo 2
+
+//Para ejecutar este programa debemos poner en consola:
+//node programaEjemplo.js
+
+//Tema ----> Utilerías en JavaScript
+
+//Llamado de la función numAleatorio del programa "utileriasJavascript.js"
+const numAleatorio = require('./utileriasJavascript.js');
+//Impresión de número aleatorio
+console.log(numAleatorio.numAleatorio());
