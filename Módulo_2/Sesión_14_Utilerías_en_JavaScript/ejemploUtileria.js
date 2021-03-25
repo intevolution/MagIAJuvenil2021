0b5c9f97bf14b2fc618b540a5797b49076f9379b
egarridoIntevolution <79933799+egarridoIntevolution@users.noreply.github.com>
2021-03-25 14:06:01 -0600

    Create ejemploUtileria.js

---
+++ Módulo_2/Sesión_14_Utilerías_en_JavaScript/ejemploUtileria.js
@@ -0,0 +1,10 @@
+//Código de la Sesión #14 Parte 1 del Módulo 2
+
+//Este programa no se va a ejecutar
+
+//Tema ----> Utilerías en JavaScript
+
+//Función que genera un número aleatorio entre 0 y 10
+exports.numAleatorio = () => {
+    return resultado = Math.round( Math.random()*10 );
+}
