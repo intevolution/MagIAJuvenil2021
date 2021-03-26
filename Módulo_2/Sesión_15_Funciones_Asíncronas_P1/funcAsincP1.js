8ec34a17e2734d1563635d915453002595df4d1f
egarridoIntevolution <79933799+egarridoIntevolution@users.noreply.github.com>
2021-03-26 00:41:03 -0600

    Update funcAsincP1.js

--- Módulo_2/Sesión_15_Funciones_Asíncronas_P1/funcAsincP1.js
+++ Módulo_2/Sesión_15_Funciones_Asíncronas_P1/funcAsincP1.js
@@ -5,16 +5,24 @@
 
 //Tema ----> Funciones Asíncronas Parte 1
 
+//Una función asíncrona es una función que se ejecuta de manera 
+//independiente del flujo de ejecución, no sabemos en que momento
+//obtendremos el valor ya que se relega la tarea a servicios
+//externos, una vez que se realicen las acciones de la función, 
+//devolverá un resultado.
+
 //Ejecución normal de JavaScript
 /*
 //Todo esto comentado son funciones normales
-//ya vistas a lo largo de la clase
+//ya vistas a lo largo de la clase.
 console.log( "Inicio" );
 function dos(){
   console.log( "Dos" );
 }
 function uno(){
   console.log( "Uno" );
+  //Si lo requerimos, podemos llamar una función dentro de otra
+  //y esta ejecutará su respectiva funcionalidad
   dos();
   console.log( "Tres" );
 }
@@ -26,6 +34,7 @@ console.log( "Fin" );
 //se encuentran dentro de las capacidades de JavaScript 
 //(en nuestro caso Nodejs), el curso de ejecución es el esperado.
 
+//Resultado:
 //Inicio
 //Uno
 //Dos
@@ -38,7 +47,7 @@ console.log( "Inicio" );
 //Esta función utiliza una funcionalidad
 //que por defecto no se encuentra en JavaScript
 //esta es "setTimeout", para que se realice lo que
-//deseamos que es "esperar una cantidad de segundos"
+//deseamos, que es "esperar una cantidad de segundos"
 //necesitamos de un servicio externo, se relegará 
 //este trabajo a ese servicio y se seguirá ejecutando 
 //instrucciones que si se puedan realizar.
@@ -53,6 +62,7 @@ function uno(){
   setTimeout( function(){
     console.log( "Uno" );
   }, 0 );
+  //Llamamos una función dentro de otra
   dos();
   console.log( "Tres" );
 }
@@ -70,4 +80,5 @@ console.log( "Fin" );
 //Esto pasa porque la ejecución para imprimir "Uno" y "Dos"
 //no la está realizando JavaScript(Nodejs) sino otro servicio,
 //en lo que se mandan los datos y se regresa la respuesta
-//ya se ejecutaron las demás instrucciones, en este caso "Fin"
+//ya se ejecutaron las demás instrucciones, en este caso 
+//la instrucción para imprimir "Fin"
