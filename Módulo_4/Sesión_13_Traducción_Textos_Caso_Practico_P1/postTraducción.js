fb99b1c6f55595d8e28d82ead3a25d59e1c3c44c
egarridoIntevolution <79933799+egarridoIntevolution@users.noreply.github.com>
2021-05-11 23:54:28 -0500

    Create postTraducción.js

---
+++ Módulo_4/Sesión_13_Traducción_Textos_Caso_Practico_P1/postTraducción.js
@@ -0,0 +1,32 @@
+//Código de la Sesión #13 del Módulo 4
+
+//Petición POST al servicio de traducción de textos
+
+//Para ejecutar este programa debemos poner en consola:
+//node postTraducción.js
+
+//Tema ----> Reconocimiento Facial Caso práctico
+
+//Llamamos a la librería axios para realizar peticiones
+const axios = require( 'axios' );
+
+//Declaramos la variable que guardará el texto a traducir
+var datos = [{"Text" : "Aquí va un texto en algún idioma disponible"}];
+
+//Guardamos la dirección del servicio ( endpoint, punto de acceso ) en una variable
+var direccion = 'Aquí va la llave del servicio';
+
+//Con axios realizamos la petición POST
+axios.post( direccion, datos, {
+    //Dentro de estos atributos debemos definir el atributo de la llave y el tipo de info que se mandará
+    headers : {
+        //Valor de la llave del servicio 
+        'Ocp-Apim-Subscription-Key': 'Aquí va la llave del servicio',
+        //La región donde se encuentra el servicio
+        'Ocp-Apim-Subscription-Region' : 'southcentralus',
+        'Content-Type' : 'application/json'  
+    }
+})
+//Accedemos al atributo que contiene el texto traducido
+.then( respuesta => console.log( respuesta.data[0].translations[0].text ))
+.catch( error => console.log( error ));
