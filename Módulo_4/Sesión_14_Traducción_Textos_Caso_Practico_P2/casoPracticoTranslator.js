d87c47daabc6b5040801f15fffc63ed2ee5d82f6
egarridoIntevolution <79933799+egarridoIntevolution@users.noreply.github.com>
2021-05-12 00:15:31 -0500

    Create casoPracticoTranslator.js
    Planteamiento del caso práctico

---
+++ Módulo_4/Sesión_14_Traducción_Textos_Caso_Practico_P2/casoPracticoTranslator.js
@@ -0,0 +1,20 @@
+//Código de la Sesión #14 del Módulo 4
+
+//Petición POST a múltiples funcionalidades del servicio de traducción
+
+//Para ejecutar este programa debemos poner en consola:
+//node casoPracticoTranslator.js
+
+//Tema ----> Traducción de textos caso práctico
+
+//Crear un programa para que Spencer pueda ayudar 
+//a los pasajeros dando instrucciones sobre donde 
+//deben de ir dependiendo de la pregunta inicial, 
+//estas instrucciones deben estar en el idioma que 
+//el pasajero usó para su pregunta inicial.
+
+//Saludos en distintos idiomas
+//console.log( "Hola, ¿Cuál es tu duda?" );
+//console.log( "Hello, what is your question?" );
+//console.log( "Bonjour, quelle est ta question?" );
+//console.log( "こんにちは、あなたの質問は何ですか？" );
